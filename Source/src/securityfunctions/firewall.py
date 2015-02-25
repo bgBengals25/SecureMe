8e21ce16fd27ebf7452ed3a69bf3462cd9c1e71a
Peter Toth <peter@ptoth585@gmail.com>
2015-02-25 15:48:29 -0500

    Fixed git problem

--- Source/src/securityfunctions/firewall.py
+++ Source/src/securityfunctions/firewall.py
@@ -4,13 +4,10 @@ import os, subprocess
 class Firewall():
 	def enable(self):
 		print('Enabling Universal Firewall...')
-<<<<<<< HEAD
 		subprocess.Popen(['/usr/bin/pkexec', "ufw enable"])
-		print('Firewall Enabled!")
-=======
+		print('Firewall Enabled!')
 		os.system("sudo ufw enable")
 		print('Firewall Enabled!')
->>>>>>> c5de9d2b19f39708fec84ba24e50e937b2ab78ae
 
 	def disable(self):
 		print('Disabling Universal Firewall...')
@@ -19,5 +16,3 @@ class Firewall():
 
 	def addexception(self, exception):
 		print('insert code here')
-
-
