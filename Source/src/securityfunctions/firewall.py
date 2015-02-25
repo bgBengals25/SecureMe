132bfe951d5fa6830c6e7ad5c557172d1d3f1610
Luke Dinkler <ljdinkler@gmail.com>
2015-02-24 21:18:53 -0500

    Edited Firewall

--- Source/src/securityfunctions/firewall.py
+++ Source/src/securityfunctions/firewall.py
@@ -1,14 +1,15 @@
 # Made by Luke Dinkler and Peter Toth
+import os, subprocess
 
 class Firewall():
 	def enable(self):
 		print('Enabling Universal Firewall...')
-		os.system("sudo ufw enable")
+		subprocess.Popen(['/usr/bin/pkexec', "ufw enable"])
 		print('Firewall Enabled!")
 
 	def disable(self):
 		print('Disabling Universal Firewall...')
-		os.system("sudo ufw disable")
+		subprocess.Popen(['/usr/bin/pkexec', "ufw disable"])
 		print('Firewall Disabled!')
 
 	def addexception(self, exception):
