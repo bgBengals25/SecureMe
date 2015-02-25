7e87dbe9f0f563ba8edfdc4922096a152be2be15
Luke Dinkler <ljdinkler@gmail.com>
2015-02-25 18:38:45 -0500

    Edited Guest Disable

--- Source/src/securityfunctions/guest.py
+++ Source/src/securityfunctions/guest.py
@@ -1,9 +1,16 @@
 # Made by Luke Dinkler and Peter Toth
+import admin, os
+from rootpassencryption import *
+
+UserPassword = encryption.decrypt()
 
 class GuestAccount():
 
 	def enable(self):
-		print('')
+		
 
 	def disable(self):
-		print('')
+		print("Disabling Guest Account...")
+		if os.path.exists("/usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf"):
+			admin.AdminExec('echo "allow-guest=false" >> /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf')
+ 
