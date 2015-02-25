4797b5d34285017a67b2282a9a540d79e7a2e65b
Peter Toth <peter@ptoth585@gmail.com>
2015-02-24 21:00:50 -0500

    Created basic gui

--- Source/src/gui/gui.py
+++ Source/src/gui/gui.py
@@ -2,8 +2,13 @@
 
 import Tkinter
 from Tkinter import *
+import ttk
 
 class InitGUI():
+
+	liberation_font_8 = ("Liberation Sans", 8)
+	liberation_font_10 = ("Liberation Sans", 10)
+	liberation_font_15 = ("Liberation Sans", 15)
 
 	def __init__(self):
 		self.build()
@@ -12,5 +17,20 @@ class InitGUI():
 		self.root = Tk()
 		self.root.geometry("600x500+300+300")
 		self.root.title("SecureMe")
+		self.root.resizable(width=FALSE, height=FALSE)
+
+		self.notebook = ttk.Notebook(self.root, height=500)
+		basicFrame = Frame(self.notebook)
+		usersFrame = Frame(self.notebook)
+		firewallFrame = Frame(self.notebook)
+		updateFrame = Frame(self.notebook)
+		self.notebook.add(basicFrame, text='Basic')
+		self.notebook.add(usersFrame, text='Users')
+		self.notebook.add(updateFrame, text='Updates')
+		self.notebook.add(firewallFrame, text='Firewall')
+		self.notebook.pack(fill=BOTH)
+
+		basic_Label = Label(basicFrame, text='Basic Security Settings', font=self.liberation_font_15)
+		basic_Label.pack()
 
 		self.root.mainloop()
