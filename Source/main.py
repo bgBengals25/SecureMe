c0ab65c8967e1bfcac3bf6da3917d7b6f46ba704
Peter Toth <peter@ptoth585@gmail.com>
2015-02-24 20:15:46 -0500

    Created basic gui

--- Source/main.py
+++ Source/main.py
@@ -1,1 +1,10 @@
 # Made by Luke Dinkler and Peter Toth
+
+from src.gui.gui import InitGUI
+
+def start():
+	gui = InitGUI()
+	print('Created GUI')
+
+if __name__ == 'main':
+	start()
--- Source/src/gui/gui.py
+++ Source/src/gui/gui.py
@@ -1,1 +1,16 @@
 # Made by Luke Dinkler and Peter Toth
+
+import Tkinter
+from Tkinter import *
+
+class InitGUI():
+
+	def __init__(self):
+		self.build()
+
+	def build(self):
+		self.root = Tk()
+		self.root.geometry("500x400+300+300")
+		self.root.title("SecureMe")
+
+		self.root.mainloop()
