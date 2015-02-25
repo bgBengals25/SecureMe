55135e323f15550f0809f9eb88ede14d80655e20
Luke Dinkler <ljdinkler@gmail.com>
2015-02-24 21:51:45 -0500

    Edited Firewall

--- Source/src/securityfunctions/firewall.py
+++ Source/src/securityfunctions/firewall.py
@@ -1,17 +1,20 @@
 # Made by Luke Dinkler and Peter Toth
 import os, subprocess, admin
 
+pwdfile = open("p.dat", "r") #Loads proposed password file
+UserPasswd = pwdfile.read() #Gets Password from the file
+pwdfile.close() #closes the file
 
 class Firewall():
 	def enable(self):
 		print('Enabling Universal Firewall...')
 
-		admin.AdminExec("ufw enable")
+		admin.AdminExec("ufw enable", UserPasswd)
 		print('Firewall Enabled!")
 
 	def disable(self):
 		print('Disabling Universal Firewall...')
-		admin.AdminExec("ufw disable")
+		admin.AdminExec("ufw disable", UserPasswd)
 		print('Firewall Disabled!')
 
 	def addexception(self, exception):
