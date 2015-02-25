5509ee1bdbd022701d2ff4adab58a70b5b2806e2
Luke Dinkler <ljdinkler@gmail.com>
2015-02-25 18:54:57 -0500

    Edited Users module

--- Source/src/securityfunctions/users.py
+++ Source/src/securityfunctions/users.py
@@ -1,9 +1,17 @@
 # Made by Luke Dinkler and Peter Toth
+import admin, os 
+from rootpassencryption import *
+
+UserPassword = encryption.decrypt()
 
 class Users():
 
 	def __init__(self):
 		print('')
+
+	def deluser(self, user):
+		admin.AdminExec("userdel -r " + user, UserPassword)
+		
 
 
 
