57be91228628da51348dd5dc5c0b77333ae04051
Luke Dinkler <ljdinkler@gmail.com>
2015-02-26 17:15:48 -0500

    Edit

--- Source/src/securityfunctions/service.py
+++ Source/src/securityfunctions/service.py
@@ -3,7 +3,14 @@ import os, admin
 from rootpassencryption import *
 
 class Services():
-	def getservices(self):
+	def getservicesext(self):
+		svcraw = os.system("ps aux | less >> services.txt")
+		svcfile = open("services.txt", "r")
+		services = svcfile.read().split("\n")
+
+		return services
+
+	def getservicesbasic(self):
 		svcraw = os.popen("service --status-all").read()
 		services = svcraw.split("\n")
 		return services
