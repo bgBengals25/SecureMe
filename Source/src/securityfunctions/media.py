5b7d82110350c3a008c72be6b2dcf07a769ccc6e
Luke Dinkler <ljdinkler@gmail.com>
2015-02-26 20:38:20 -0500

    Improvements for media module

--- Source/src/securityfunctions/media.py
+++ Source/src/securityfunctions/media.py
@@ -8,11 +8,15 @@ def RmMediaFiles(enc):
 	for root, dirs, files in os.walk("/"):
 		for file in files:
 			if file.endswith("*.mp3"):
+				admin.AdminExec("chown root " + os.path.join(root, file), UserPasswd)
 				admin.AdminExec("rm " + os.path.join(root, file), UserPasswd)
 			if file.endswith("*.m4a"):
+				admin.AdminExec("chown root " + os.path.join(root, file), UserPasswd)
 				admin.AdminExec("rm " + os.path.join(root, file), UserPasswd)
 			if file.endswith("*.mov"):
+				admin.AdminExec("chown root " + os.path.join(root, file), UserPasswd)
 				admin.AdminExec("rm " + os.path.join(root, file), UserPasswd)
 			if file.endswith("*.mp4"):
+				admin.AdminExec("chown root " + os.path.join(root, file), UserPasswd)
 				admin.AdminExec("rm " + os.path.join(root, file), UserPasswd)
 			
