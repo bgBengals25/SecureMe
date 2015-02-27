68b4b30db8eb872175492034085a57ff9c464669
Peter Toth <peter@ptoth585@gmail.com>
2015-02-26 20:13:03 -0500

    fixed service.py

--- Source/src/gui/gui.py
+++ Source/src/gui/gui.py
@@ -4,7 +4,8 @@ import Tkinter
 from Tkinter import *
 import ttk
 from getrtpwd import RootPasswordWindow
-import os
+import os, sys
+from gt import GroupsText
 os.system('cd ..')
 from src.securityfunctions.admin import *
 from src.securityfunctions.firewall import *
@@ -26,13 +27,20 @@ class InitGUI():
 
 	def build(self):
 		self.root = Tk()
-		self.root.geometry("500x300+300+300")
+		self.root.geometry("500x400+300+300")
 		self.root.title("SecureMe")
 		self.root.resizable(width=FALSE, height=FALSE)
 
+		menubar = Menu(self.root)
+		optionsmenu = Menu(menubar)
+		optionsmenu.add_command(label="Refresh", command=lambda : self.refresh())
+		optionsmenu.add_command(label="Exit", command=sys.exit)
+		menubar.add_cascade(label="Options", menu=optionsmenu)
+		self.root.config(menu=menubar)
+
 		self.notebook = ttk.Notebook(self.root, height=500)
 		basicFrame = Frame(self.notebook)
-		usersFrame = Frame(self.notebook)
+		usersFrame = Frame(self.notebook, padx=25, pady=25)
 		firewallFrame = Frame(self.notebook)
 		updateFrame = Frame(self.notebook)
 		self.notebook.add(basicFrame, text='Basic')
@@ -51,12 +59,21 @@ class InitGUI():
 		users_label = Label(usersFrame, text='User Security Settings', font=self.liberation_font_15)
 		users_label.pack()
 
-		userpanel = LabelFrame(usersFrame, text="Users:", padx=10, pady=10)
-		userpanel.pack(side=TOP)
+		userpanel = LabelFrame(usersFrame, text="Users", padx=10, pady=10)
+		userpanel.pack(side=TOP, fill=BOTH)
 
-		uText = self.getUserText()
-		users_listlabel = Label(userpanel, text=uText, padx=10, pady=10)
-		users_listlabel.pack(side=LEFT)
+		self.uText = self.getUserText()
+		self.users_listlabel = Label(userpanel, text=self.uText, padx=10, pady=10)
+		self.users_listlabel.pack(side=LEFT)
+
+		groupspanel = LabelFrame(usersFrame, text="Groups", padx=10, pady=10)
+		groupspanel.pack(side=TOP, fill=BOTH)
+
+		self.gText = self.getGroupText()
+		self.groups_text = GroupsText(groupspanel)
+		self.groups_text.resetText(self.gText)
+		self.groups_text.type(DISABLED)
+		self.groups_text.pack(fill=BOTH)
 
 		# Firewall Label
 		firewall_label = Label(firewallFrame, text='Firewall Settings', font=self.liberation_font_15)
@@ -77,6 +94,14 @@ class InitGUI():
 
 		self.root.mainloop()
 
+	def refresh(self):
+		self.uText = self.getUserText()
+		self.gText = self.getGroupText()
+		self.users_listlabel.config(text=self.uText)
+		self.groups_text.type(NORMAL)
+		self.groups_text.resetText(self.gText)
+		self.groups_text.type(DISABLED)
+
 	def getPassword(self):
 		pwd = self.enc.decrypt()
 		return pwd
@@ -87,6 +112,14 @@ class InitGUI():
 		ret = ''
 		for i in retstr:
 			ret += "User: " + i + "\n"
+		return ret
+
+	def getGroupText(self):
+		g = Groups()
+		retstr = g.getGroups()
+		ret = ''
+		for i in retstr:
+			ret += i + "\n"
 		return ret
 
 	def basicUpdate(self):
--- Source/src/securityfunctions/service.py
+++ Source/src/securityfunctions/service.py
@@ -11,13 +11,12 @@ class Services():
 		return services
 
 	def getservicesbasic(self):
-		os.system('echo service --status-all >>')
 		svcraw = os.popen("service --status-all").read()
 		services = svcraw.split("\n")
 		return services
 	
 	def getservicesalt(self):
-		os.system("echo service --status all >> myservices.txt")
+		os.system("service --status all echo >> myservices.txt")
 		svcfile = open("myservices.txt", "r")
 		services = svcfile.read().split("\n")
 		return services
