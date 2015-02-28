72e16cf67d3995e624b0115de7bd697c945ba1da
Luke Dinkler <ljdinkler@gmail.com>
2015-02-27 20:13:19 -0500

    Added virus scan

--- Source/src/securityfunctions/antivirus.py
+++ Source/src/securityfunctions/antivirus.py
@@ -22,6 +22,16 @@ class AntiVirus():
 	def update(self):
 		admin.AdminExec("freshclam", UserPasswd)
 	
+	def full_scan(self):
+		os.system("clamscan -r /")
+	
+	def home_scan(self):
+		os.system("clamscan -r /home")
+	
+	def search_and_destroy(self):
+		os.system("clamscan -r --remove /")
+	
+	
 class RootKits():
 	def install(self):
 		admin.AdminExec("apt-get install rkhunter -y", UserPasswd)
