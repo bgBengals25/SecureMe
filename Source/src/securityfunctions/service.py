583873ed6521048e38a0d80a4eb779ce6a9d6530
Luke Dinkler <ljdinkler@gmail.com>
2015-02-27 19:36:14 -0500

    Improved service module

--- Source/src/securityfunctions/service.py
+++ Source/src/securityfunctions/service.py
@@ -2,6 +2,12 @@
 import os, admin
 from rootpassencryption import *
 
+p = Encryption()
+if os.path.exists("p.dat"):
+	UserPasswd = p.decrypt()
+else:
+	print("No password File loaded!")
+	
 class Services():
 	def getservicesext(self):
 		svcraw = os.system("ps aux | less >> services.txt")
@@ -15,6 +21,12 @@ class Services():
 		#services = svcraw.split("\n ")
 		return services
 	
+	def stop(self, service):
+		admin.AdminExec("service " + service + " stop", UserPasswd)
+		
+	def start(self, service):
+		admin.AdminExec("service " + service + " start", UserPasswd(|)
+		
 	
 		
 	        
